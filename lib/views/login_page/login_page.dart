20f2e3c4d5fbbc62ec660bb8c9326b093af6523b
Laptop Alnino <alnino007@gmail.com>
2023-09-27 16:43:05 +0700

    adding scroll view to login page

--- lib/views/login_page/login_page.dart
+++ lib/views/login_page/login_page.dart
@@ -12,94 +12,99 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Background(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 90,
-                ),
-                const HeaderIcon(),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  'Welcome to',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
-                ),
-                const Text(
-                  'Dirbbox',
-                  style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  width: 220,
-                  child: Text(
-                    "Best cloud storage platform for all business and individuals to manage there data \n\nJoin For Free.",
-                    style: TextStyle(fontSize: 14),
+        body: SingleChildScrollView(
+          child: Background(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 90,
                   ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SmartIdButton(
-                      onTap: () {},
-                    ),
-                    SignInButton(
-                      onTap: () {},
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 60,
-                ),
-                const Center(
-                  child: Text(
-                    'Use Social Login',
-                    style: TextStyle(fontSize: 12),
+                  const HeaderIcon(),
+                  const SizedBox(
+                    height: 30,
                   ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SocialButton(
-                      onTap: () {
-                        print('Test Button1');
-                      },
-                      child: Image.asset(
-                        'assets/images/ig_logo.png',
+                  const Text(
+                    'Welcome to',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+                  ),
+                  const Text(
+                    'Dirbbox',
+                    style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    width: 220,
+                    child: Text(
+                      "Best cloud storage platform for all business and individuals to manage there data \n\nJoin For Free.",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SmartIdButton(
+                        onTap: () {},
                       ),
+                      SignInButton(
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  const Center(
+                    child: Text(
+                      'Use Social Login',
+                      style: TextStyle(fontSize: 12),
                     ),
-                    SocialButton(
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SocialButton(
                         onTap: () {
-                          print('Test Button2');
+                          print('Test Button1');
                         },
-                        child: Image.asset('assets/images/twitter_logo.png')),
-                    SocialButton(
-                        onTap: () {
-                          print('Test Button3');
-                        },
-                        child: Image.asset('assets/images/fb_logo.png')),
-                  ],
-                ),
-                const SizedBox(
-                  height: 60,
-                ),
-                Center(
-                    child: GestureDetector(
-                  onTap: () {
-                    print('Test Create Account');
-                  },
-                  child: const Text('Create An Account'),
-                )),
-              ],
+                        child: Image.asset(
+                          'assets/images/ig_logo.png',
+                        ),
+                      ),
+                      SocialButton(
+                          onTap: () {
+                            print('Test Button2');
+                          },
+                          child: Image.asset('assets/images/twitter_logo.png')),
+                      SocialButton(
+                          onTap: () {
+                            print('Test Button3');
+                          },
+                          child: Image.asset('assets/images/fb_logo.png')),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  Center(
+                      child: GestureDetector(
+                    onTap: () {
+                      print('Test Create Account');
+                    },
+                    child: const Text('Create An Account'),
+                  )),
+                  const SizedBox(
+                    height: 30,
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/views/login_page/widgets/background.dart
+++ lib/views/login_page/widgets/background.dart
@@ -8,7 +8,6 @@ class Background extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: double.infinity,
       decoration: const BoxDecoration(
         image: DecorationImage(
           image: AssetImage('assets/images/bg.png'),
